018993ae4629973e7f1923bb5aef038148b7fdc0
yamanoku <0910yama@gmail.com>
2025-10-26 15:29:24 +0900

    chore: deploy

--- slide/assets/slidev/center-D8_PfbzN.js
+++
@@ -1,1 +0,0 @@
-import{f as t,o,g as s,j as c}from"../modules/vue-S3NNZXlQ.js";import{_ as r}from"../index-C7AnWt2g.js";const a={},n={class:"slidev-layout center h-full grid place-content-center"},l={class:"my-auto"};function d(e,_){return o(),t("div",n,[s("div",l,[c(e.$slots,"default")])])}const m=r(a,[["render",d]]);export{m as I};
